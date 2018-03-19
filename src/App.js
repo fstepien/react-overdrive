d38605fa3bf92df8dc12f22b0f59f5a55f4369cb
Filip Stepien <contact@filipstepien.com>
2018-03-19 07:28:19 -0400

    initial movie components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
 
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'},
+  {
+    id: 2,
+    title: 'Spider Man'},
+  {
+    id: 3,
+    title: 'Last Movie'},
+]
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {movies.map(movie=>(
+            <Movie key={movie.id} movie={movie}/>
+          )
+        )}
       </div>
     );
   }
