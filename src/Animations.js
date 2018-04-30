df2ff6d3e77ee8706a683b7df8463f7b059cb370
Filip Stepien <contact@filipstepien.com>
2018-04-30 10:22:54 -0400

    slide updates css/js animations

--- src/Animations.js
+++ src/Animations.js
@@ -2,15 +2,33 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import left from './caret-square-left.svg';
 import right from './caret-square-right.svg';
-import pack from './pack.png';
+import packTop from './pack-top.png';
+import packBottom from './pack-bottom.png';
+import animations from './animations.png';
+import unlock from './unlock.svg';
 
 class Transitions extends Component {
 	render() {
 		return (
 			<React.Fragment>
 				<section className="slide welcome">
-					<h2>CSS TRANSITIONS</h2>
-					<img src={pack} className="pack" alt="pink hip pack" />
+					<h2>CSS ANIMATIONS</h2>
+					<img src={unlock} className="lock unlock" alt="unlocked pad lock" />
+					<img
+						src={animations}
+						className="animation-code"
+						alt="css for animations"
+					/>
+					<img
+						src={packTop}
+						className="pack-css-top"
+						alt="pink hip pack top zip"
+					/>
+					<img
+						src={packBottom}
+						className="pack pack-css pack-css-bottom"
+						alt=""
+					/>
 				</section>
 				<Link to={'/'}>
 					<img
--- src/JavaScript.js
+++ src/JavaScript.js
@@ -2,22 +2,25 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import left from './caret-square-left.svg';
 import right from './caret-square-right.svg';
-import pack from './pack.png';
-import reactImage from './React.png';
+import packTop from './pack-top.png';
+import packBottom from './pack-bottom.png';
 
 class JavaScript extends Component {
 	render() {
 		return (
 			<React.Fragment>
 				<section className="slide react">
-					<h2>JavaScript</h2>
-					<p>
-						Components let you split the UI into independent, reusable pieces,
-						and think about each piece in isolation.
-					</p>
-					<div className="image">
-						<img src={reactImage} alt="" />
-					</div>
+					<h2>JAVASCRIPT</h2>
+					<img
+						src={packTop}
+						className="pack pack-top"
+						alt="pink hip pack top zip"
+					/>
+					<img
+						src={packBottom}
+						className="pack"
+						alt="pink hip pack main pack"
+					/>
 				</section>
 				<Link to={'/Animations'}>
 					<img
